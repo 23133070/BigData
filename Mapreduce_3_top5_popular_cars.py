11bbb261029bae3ef5418e11a5a02f64149af209
phuquy020105-oss <phuquy020105@gmail.com>
2025-10-12 21:59:46 +0700

    Update Mapreduce_3_top5_popular_cars.py

--- Mapreduce_3_top5_popular_cars.py
+++ Mapreduce_3_top5_popular_cars.py
@@ -2,13 +2,15 @@ from pyspark.sql import SparkSession, functions as F
 
 spark = SparkSession.builder.appName("Top5Cars").getOrCreate()
 
-df = spark.read.option("header","true").csv("hdfs://192.168.110.102:9000/input/car_data_clean.csv")
+# Đọc Parquet từ HDFS
+df = spark.read.parquet("hdfs://192.168.110.105:9000/user/phuquy/BigData_Project/StandardizedDataCar/car_data_normalized.parquet")
+
 df = df.withColumnRenamed("tên_xe","car_name")
 
 res = df.groupBy("car_name").count().orderBy(F.desc("count")).limit(5)
 #res.show(20, truncate=False)
 
-res.write.mode("overwrite").parquet("hdfs://192.168.110.102:9000/output/top5_cars")
+res.write.mode("overwrite").parquet("hdfs://192.168.110.105:9000/output/top5_cars")
 
 print(" MapReduce Top5Cars đã chạy xong. Kiểm tra HDFS /output/top5_cars")
 
